f1038a1f56c044a31452d006a84d272fb883fc17
Krishiv <47680902+krishiv8190@users.noreply.github.com>
2020-10-08 11:03:03 +0530

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 # write commands if you want to clone some repos
+
+git clone https://github.com/krishiv8190/device_xiaomi_sakura device/xiaomi/sakura
+git clone https://github.com/mahajant99/vendor_xiaomi vendor/xiaomi
+git clone https://github.com/darkhz/revvz_sakura kernel/xiaomi/msm8953
